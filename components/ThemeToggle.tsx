680e54e26df2c6ec9ff8ac35772322611b926a4f
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-12-11 13:34:43 +0300

    fix: Update themetoggle.tsx

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -11,7 +11,7 @@ export function ThemeToggle() {
       variant="ghost"
       size="sm"
       onClick={toggleTheme}
-      className="absolute p-2 top-4 right-4 z-500 bg-transparent"
+      className="absolute p-2 top-4 right-[40%] z-500 bg-transparent"
       title={darkMode ? 'Switch to light mode' : 'Switch to dark mode'}
     >
       <Sun className={`h-5 w-5 text-black transition-all ${darkMode ? 'scale-0 rotate-90' : 'scale-100 rotate-0'}`} />
